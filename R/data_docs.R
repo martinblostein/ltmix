196f812cdc313f3d8926ce4c9c1166baad433361
Martin Blostein <martin.blostein@gmail.com>
2019-03-18 22:48:10 -0700

    Add documentation for secura re dataset

---
+++ R/data_docs.R
@@ -0,0 +1,6 @@
+#' The Secura Belgian Re Data
+#'
+#' @description "The Secura Belgian Re data set contains automobile claims from 1988 until 2001, which are at least as large as 1,200,000 Euros." (Beirlant, Goegebeur, Segers & Teugels, 2004).
+#' @references Beirlant, J., Goegebeur Y., Segers, J., & Teugels, J. Statistics of extremes : theory and applications. Hoboken, NJ: Wiley, 2004. Print.
+#' @references \url{https://lstat.kuleuven.be/Wiley/}
+"secura"
