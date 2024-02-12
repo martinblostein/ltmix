1b4ffcc818861a6e3da160ea7d492cb88a3fd9e0
Martin Blostein <martin.blostein@innovasea.com>
2024-02-11 19:29:38 -0800

    Regenerate doxygen2 documentation & address '@docType package' deprecation warning

--- R/ltmix-package.R
+++ R/ltmix-package.R
@@ -1,7 +1,7 @@
 #' ltmix: Left-Truncated Mixtures of Gamma, Weibull, and Lognormal Distributions
 #'
 #' @name ltmix
-#' @docType package
+#' @aliases _PACKAGE
 #' @import stats
 #' @description Mixture modelling of one-dimensional data using combinations of left-truncated Gamma,
 #' Weibull, and Lognormal Distributions.
